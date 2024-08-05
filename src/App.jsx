921d4646aa9f10bc9dbfa3dfc5a41d1fa238e749
Darshan <darsuprajapati262@gmail.com>
2024-08-05 08:33:42 +0530

    Add h2 tag

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Hiii</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
